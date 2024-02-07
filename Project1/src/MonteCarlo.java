89293680a96e9506c0f0a6e4183559e0dc44445b
melvi <melvi@134.210.211.133>
2024-02-06 19:05:50 -0500

    Fixed code

--- Project1/src/MonteCarlo.java
+++ Project1/src/MonteCarlo.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MonteCarlo {
+	private ArrayList<Card> deck; // This is the constructors job. = new Card[];
+	private ArrayList<Card> hand;
+	private int pokemonCount;
+	private int trueCount;
+	private int falseCount;
+	private int deckSize = 60;
 	
+	public MonteCarlo() {
+		deck = new ArrayList<Card>();
+		hand = new ArrayList<Card>();
+	}
+	public Card drawCard() {
+		Random rng = new Random();
+		int cardIndex = rng.nextInt(deck.size()); // Find Random card
+		Card drawnCard = deck.get(cardIndex);
+		deck.remove(cardIndex);
+		return drawnCard;
+	}
+	public void drawHand() {
+		for (int i = 0; i < 7; i++) {// We're counting to 7
+			hand.add(drawCard());
+		}
+	}
 	public void constructDeck(int numOfPokemon){
 		deck.clear();
 		hand.clear();
@@ -13,11 +37,22 @@ public class MonteCarlo {
 			deck.add(new Energy());
 		}
 	}
+	public boolean evaluateOpeningHand(){
+		boolean havePokemon = false;
+		for (int i = 0; i < hand.size(); i++) {
+			Card currentCard = hand.get(i);
+			if (currentCard instanceof Pokemon) {
+				trueCount++;
+				return true;
+				
+			}
+		}
+		falseCount++;
+		return false;
+	}
 	
 	//Make engine for program
 	public void run(){
-		constructDeck();
-		while(evaluateOpeningHand() == false)
 		for(int i = 0; i < deckSize; i++){
 			trueCount = 0;
 			falseCount = 0;
